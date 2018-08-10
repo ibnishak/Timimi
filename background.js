82c6e6504ca1eab180db17784a77e4f1fd3bb476
ibnishak <ibnishak@protonmail.com>
2018-08-10 10:43:41 +0530

    Change the code to onclick

--- background.js
+++ background.js
@@ -6,18 +6,22 @@ var portFromCS;
 // This function is executed on connection
 function connected(p) {
   portFromCS = p;
-  browser.browserAction.onClicked.addListener(function() {
-	  portFromCS.postMessage({greeting: "hi there content script!"});
-	  portFromCS.onMessage.addListener(function(m) {
-	    console.log("In background script, received message from content script")
-	    console.log(m.greeting);
-	  });
-  });
+  // browser.browserAction.onClicked.addListener(function() {
+	 //  portFromCS.postMessage({greeting: "hi there content script!"});
+	 //  portFromCS.onMessage.addListener(function(m) {
+	 //    console.log("In background script, received message from content script")
+	 //    console.log(m.greeting);
+	 //  });
+  // });
 }
 
 // Actual command to execute the connected function
 browser.runtime.onConnect.addListener(connected);
 // Pass message to content script on clicking the button. Need response here
-// browser.browserAction.onClicked.addListener(function() {
-//   portFromCS.postMessage({greeting: "they clicked the button!"});
-// });
+browser.browserAction.onClicked.addListener(function() {
+  	portFromCS.postMessage({greeting: "they clicked the button!"});
+	portFromCS.onMessage.addListener(function(m) {
+    console.log("In background script, received message from content script")
+    console.log(m.greeting);
+  });
+});
