96d01f24d058476b41ca65a2a4a45f78ec7932f4
ibnishak <ibnishak@protonmail.com>
2018-08-10 10:16:48 +0530

    Init

---
+++ background.js
@@ -0,0 +1,21 @@
+// background-script.js
+
+var portFromCS;
+
+
+// This function is executed on connection
+function connected(p) {
+  portFromCS = p;
+  portFromCS.postMessage({greeting: "hi there content script!"});
+  portFromCS.onMessage.addListener(function(m) {
+    console.log("In background script, received message from content script")
+    console.log(m.greeting);
+  });
+}
+
+// Actual command to execute the connected function
+browser.runtime.onConnect.addListener(connected);
+// Pass message to content script on clicking the button. Need on response here
+browser.browserAction.onClicked.addListener(function() {
+  portFromCS.postMessage({greeting: "they clicked the button!"});
+});
---
+++ content-script.js
@@ -0,0 +1,14 @@
+// content-script.js
+// Message posted as soon as connected
+var myPort = browser.runtime.connect({name:"port-from-cs"});
+myPort.postMessage({greeting: "hello from content script"});
+// This will be carried out in response to message from background js. Including onclick
+myPort.onMessage.addListener(function(m) {
+  console.log("In content script, received message from background script: ");
+  console.log(m.greeting);
+ // myPort.postMessage({greeting: "Now we are sending messages back"});
+});
+
+document.body.addEventListener("click", function() {
+  myPort.postMessage({greeting: "they clicked the page!"});
+});
