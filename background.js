9a59e265d2530ea3f99632e434420149a8f7b7ae
ibnishak <ibnishak@protonmail.com>
2018-08-11 10:35:08 +0530

    Post message solved

--- background.js
+++ background.js
@@ -1,50 +1,17 @@
-// background-script.js
-
 var portFromCS;
 
-
-// This function is executed on connection
 function connected(p) {
   portFromCS = p;
-  // browser.browserAction.onClicked.addListener(function() {
-	 //  portFromCS.postMessage({greeting: "hi there content script!"});
-	 //  portFromCS.onMessage.addListener(function(m) {
-	 //    console.log("In background script, received message from content script")
-	 //    console.log(m.greeting);
-	 //  });
-  // });
-}
-
-
-function onResponse(response) {
-    console.log("Received " + response);
-    browser.notifications.create({
-	"type": "basic",
-	"title": response,
-	"message": "Link added to shiori",
-	"iconUrl": browser.extension.getURL("icons/shiori.png")
-  });
-}
-
-function onError(error) {
-    console.log(`Error: ${error}`);
-}
-
-// Actual command to execute the connected function
-browser.runtime.onConnect.addListener(connected);
-// Pass message to content script on clicking the button. Need response here
-browser.browserAction.onClicked.addListener(function() {
-  portFromCS.postMessage({msg: "Calling save modules"});
-	portFromCS.onMessage.addListener(function(m) {
+  portFromCS.postMessage({greeting: "hi there content script!"});
+  portFromCS.onMessage.addListener(function(m) {
     console.log("In background script, received message from content script")
-    console.log(m);
-    // console.log(m.docbody);
-    // console.log(m.dochead);
-    // console.log(m.docurl);
- //    var sending = browser.runtime.sendNativeMessage(
-	//      "keeptw",
-	//      m);
-	// sending.then(onResponse, onError);
+    console.log(m.greeting);
   });
-});
+}
 
+browser.runtime.onConnect.addListener(connected);
+
+
+// On connect  -> bg posts hi there, bg receieves a reply - bg consoles it
+// then the power moves to content script - it consoles and posts message
+// once bg receives the message it posted - it consoles it
