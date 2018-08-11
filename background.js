06380d500d460c6a66e07e7b7434361510e31774
ibnishak <ibnishak@protonmail.com>
2018-08-11 23:11:08 +0530

    Solving "multiple-save" bug, adding port disconnect

--- background.js
+++ background.js
@@ -1,20 +1,26 @@
 var portFromCS;
 
 function connected(p) {
-  portFromCS = p;
-  portFromCS.postMessage({message: "Initiate"});
-  portFromCS.onMessage.addListener(function(m) {
-    console.log("Saving to" + m.path);
-  var sending = browser.runtime.sendNativeMessage(
-       "keeptw",m);
-  sending.then(onResponse, onError);
-  });
+    portFromCS = p;
+    portFromCS.postMessage({ message: "Initiate" });
+    portFromCS.onMessage.addListener(function(m) {
+        console.log("Saving to " + m.path);
+        var sending = browser.runtime.sendNativeMessage(
+            "keeptw", m);
+        sending.then(onResponse, onError);
+    });
+    portFromCS.onDisconnect.addListener(disconnected);
 }
+
+function disconnected() {
+    console.log("Not a local Tiddlywiki file");
+}
+
 
 browser.runtime.onConnect.addListener(connected);
 
 function onResponse(response) {
-    portFromCS.postMessage({message: "Saved-Successfully"});
+    console.log("Saved Successfully");
 }
 
 function onError(error) {
--- content-script.js
+++ content-script.js
@@ -1,34 +1,62 @@
-
-var myPort = browser.runtime.connect({name:"port-from-cs"});
+var twport = browser.runtime.connect({ name: "port-from-cs" });
 var idGenerator = 1;
 
-var messageBox = document.getElementById("tiddlyfox-message-box");
-if(!messageBox) {
-	messageBox = document.createElement("div");
-	messageBox.id = "tiddlyfox-message-box";
-	messageBox.style.display = "none";
-	document.body.appendChild(messageBox);
+
+function checkTW() {
+    var results = {};
+    // Test for TiddlyWiki Classic
+    var versionArea = document.getElementById("versionArea");
+    results.isTiddlyWikiClassic = (document.location.protocol === "file:") &&
+        document.getElementById("storeArea") &&
+        (versionArea && /TiddlyWiki/.test(versionArea.textContent));
+    // Test for TiddlyWiki 5
+    var metaTags = document.getElementsByTagName("meta");
+    for (var t = 0; t < metaTags.length; t++) {
+        if (metaTags[t].name === "application-name" && metaTags[t].content === "TiddlyWiki") {
+            results.isTiddlyWiki5 = true;
+        }
+    }
+    results.isTiddlyWiki = results.isTiddlyWikiClassic || results.isTiddlyWiki5;
+    // Test for file URI
+    if (document.location.protocol === "file:") {
+        results.isLocalFile = true;
+    }
+    return results;
 }
 
-myPort.onMessage.addListener(function(m) {
-  console.log(m.message);
-	// Attach the event handler to the message box
-	messageBox.addEventListener("tiddlyfox-save-file",onSaveTiddlyWiki,false);
+var checkTWResults = checkTW();
 
-	function onSaveTiddlyWiki(event) {
-	// Get the details from the message
-	var messageElement = event.target,
-		path = messageElement.getAttribute("data-tiddlyfox-path"),
-		content = messageElement.getAttribute("data-tiddlyfox-content"),
-		backupPath = messageElement.getAttribute("data-tiddlyfox-backup-path"),
-		messageId = "tiddlywiki-save-file-response-" + idGenerator++;
-		console.log("Saving to " + path);
-		myPort.postMessage({path: path, messageId: messageId, content: content, backupPath:backupPath});
-		messageElement.parentNode.removeChild(messageElement);
-		console.log("Saving huuuge success");
-    	var event = document.createEvent("Events");
-		event.initEvent("tiddlyfox-have-saved-file",true,false);
-		event.savedFilePath = path;
-		messageElement.dispatchEvent(event);
-		}
-});
+
+if (checkTWResults.isTiddlyWiki5 && checkTWResults.isLocalFile) {
+
+    var messageBox = document.getElementById("tiddlyfox-message-box");
+    if (!messageBox) {
+        messageBox = document.createElement("div");
+        messageBox.id = "tiddlyfox-message-box";
+        messageBox.style.display = "none";
+        document.body.appendChild(messageBox);
+    }
+    twport.onMessage.addListener(function(m) {
+        console.log(m.message);
+        // Attach the event handler to the message box
+        messageBox.addEventListener("tiddlyfox-save-file", onSaveTiddlyWiki, false);
+
+        function onSaveTiddlyWiki(event) {
+            // Get the details from the message
+            var messageElement = event.target,
+                path = messageElement.getAttribute("data-tiddlyfox-path"),
+                content = messageElement.getAttribute("data-tiddlyfox-content"),
+                backupPath = messageElement.getAttribute("data-tiddlyfox-backup-path"),
+                messageId = "tiddlywiki-save-file-response-" + idGenerator++;
+            twport.postMessage({ path: path, messageId: messageId, content: content, backupPath: backupPath });
+            messageElement.parentNode.removeChild(messageElement);
+            console.log("Message ID is " + messageId);
+            var event = document.createEvent("Events");
+            event.initEvent("tiddlyfox-have-saved-file", true, false);
+            event.savedFilePath = path;
+            messageElement.dispatchEvent(event);
+        }
+    });
+} else {
+    twport.disconnect();
+}
