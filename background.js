8f1c841e597e43d76855013416e9208460ebfe61
ibnishak <ibnishak@protonmail.com>
2018-08-11 10:13:24 +0530

    Postmessage bug

--- background.js
+++ background.js
@@ -15,13 +15,36 @@ function connected(p) {
   // });
 }
 
+
+function onResponse(response) {
+    console.log("Received " + response);
+    browser.notifications.create({
+	"type": "basic",
+	"title": response,
+	"message": "Link added to shiori",
+	"iconUrl": browser.extension.getURL("icons/shiori.png")
+  });
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
 // Actual command to execute the connected function
 browser.runtime.onConnect.addListener(connected);
 // Pass message to content script on clicking the button. Need response here
 browser.browserAction.onClicked.addListener(function() {
-  	portFromCS.postMessage({greeting: "they clicked the button!"});
+  portFromCS.postMessage({msg: "Calling save modules"});
 	portFromCS.onMessage.addListener(function(m) {
     console.log("In background script, received message from content script")
-    console.log(m.greeting);
+    console.log(m);
+    // console.log(m.docbody);
+    // console.log(m.dochead);
+    // console.log(m.docurl);
+ //    var sending = browser.runtime.sendNativeMessage(
+	//      "keeptw",
+	//      m);
+	// sending.then(onResponse, onError);
   });
 });
+
--- content-script.js
+++ content-script.js
@@ -1,14 +1,37 @@
 // content-script.js
 // Message posted as soon as connected
+var idGenerator = 1;
 var myPort = browser.runtime.connect({name:"port-from-cs"});
 myPort.postMessage({greeting: "hello from content script"});
 // This will be carried out in response to message from background js. Including onclick
 myPort.onMessage.addListener(function(m) {
-  console.log("In content script, received message from background script: ");
-  console.log(m.greeting);
-  myPort.postMessage({greeting: "Now we are sending messages back"});
+  console.log(m.msg);
+  console.log("Responding to save");
+  myPort.postMessage({docbody: document.body.outerHTML, docurl: document.URL, dochead: document.head.outerHTML});
 });
 
+var messageBox = document.getElementById("tiddlyfox-message-box");
+	if(!messageBox) {
+		messageBox = document.createElement("div");
+		messageBox.id = "tiddlyfox-message-box";
+		messageBox.style.display = "none";
+		document.body.appendChild(messageBox);
+	}
+	// Attach the event handler to the message box
+messageBox.addEventListener("tiddlyfox-save-file",onSaveTiddlyWiki,false);
 // document.body.addEventListener("click", function() {
 //   myPort.postMessage({greeting: "they clicked the page!"});
 // });
+
+
+function onSaveTiddlyWiki(event) {
+	// Get the details from the message
+	var messageElement = event.target,
+		path = messageElement.getAttribute("data-tiddlyfox-path"),
+		content = messageElement.getAttribute("data-tiddlyfox-content"),
+		backupPath = messageElement.getAttribute("data-tiddlyfox-backup-path"),
+		messageId = "tiddlywiki-save-file-response-" + idGenerator++;
+		console.log("yay");
+		console.log(path);
+		myPort.postMessage({greeting: "hello from FOX script"});
+		}
