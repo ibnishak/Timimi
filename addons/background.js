a4fbf57872a9b12597f77a03969e15b2cb77344b
ibnishak <ibnishak@protonmail.com>
2018-08-12 14:54:19 +0530

    Folder restructure. Removing hosts from xpi

---
+++ addons/background.js
@@ -0,0 +1,31 @@
+var portFromCS;
+
+function connected(p) {
+    portFromCS = p;
+    portFromCS.postMessage({ message: "Initiate" });
+    portFromCS.onDisconnect.addListener(disconnected);
+}
+
+function disconnected() {
+    console.log("Not a local Tiddlywiki file");
+}
+
+function onResponse(response) {
+    sendResponse({ response: "Saved Successfully" });
+    //console.log("Background script sends success response to content script")
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+function handleMessage(request, sender, sendResponse) {
+    console.log("Sending native message");
+    var sending = browser.runtime.sendNativeMessage(
+        "timimi", request);
+    sending.then(onResponse, onError);
+}
+
+
+browser.runtime.onConnect.addListener(connected);
+browser.runtime.onMessage.addListener(handleMessage);
