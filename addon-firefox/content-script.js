b8f1efc306e8fbfed171181bc742c651f826f4f4
Rizwan Ishak <ibnishak@live.com>
2020-04-21 09:02:27 +0530

    Removing script execution capabilities

--- addon-firefox/content-script.js
+++ addon-firefox/content-script.js
@@ -1,7 +1,6 @@
 var idGenerator = 1;
-var data = {};
+var timimisettings = {};
 var tlast = new Date();
-var indata = {};
 
 // If and only if the local file is TW-Classic(old) file, inject the patch enabling it to be saved with tiddlyfox-derived savers
 function injectExtensionScript(path) {
@@ -37,23 +36,42 @@ function checkTW() {
   }
   return results;
 }
-var checkTWResults = checkTW();
 
 // Loading settings from browser storage
-var getting = browser.storage.sync.get();
-getting.then(onGot, syncError);
-function syncError(error) {
-  console.log(`Error in getting values from browser storage: ${error}`);
-}
-function onGot(item) {
-  data = item;
-  if (data.backup == "yes") {
-    console.log("Timimi: Backups enabled");
-    console.log("Timimi: Backup method: " + data.bstrategy);
+function getSettings() {
+  var getting = browser.storage.sync.get();
+  getting.then(onGot, syncError);
+  function syncError(error) {
+    console.log(`Error in getting values from browser storage: ${error}`);
+  }
+  function onGot(item) {
+    timimisettings = item;
+    if (timimisettings.backup == "yes") {
+      console.log("Timimi: Backups enabled");
+      console.log("Timimi: Backup method: " + timimisettings.bstrategy);
+    }
   }
 }
 
+
+
+// ----- START HERE -------- //
+
+// Check if the local file is a TW
+var checkTWResults = checkTW();
+
+// If it is a TW-Classic, then do @YakovL magic to save it
+if (checkTWResults.isTiddlyWikiClassic && checkTWResults.isLocalFile) {
+  getSettings();
+  injectExtensionScript('patch-classic-io.js');
+}
+
+// If it is a TW-5, then do extract settings and save it.
 if (checkTWResults.isTiddlyWiki && checkTWResults.isLocalFile) {
+  // Extract settings
+  getSettings();
+
+  // Build an area in the TW5 where TW5 can notify a save event. Code credits to @Jermolene
   var messageBox = document.getElementById("tiddlyfox-message-box");
   if (!messageBox) {
     messageBox = document.createElement("div");
@@ -61,45 +79,46 @@ if (checkTWResults.isTiddlyWiki && checkTWResults.isLocalFile) {
     messageBox.style.display = "none";
     document.body.appendChild(messageBox);
   }
-  // Listen to initiate message from background script
   // Attach the event handler to the message box
   messageBox.addEventListener("tiddlyfox-save-file", onSaveTiddlyWiki, false);
-  messageBox.addEventListener("timimi-action-launch", onActionLaunch, false);
+
   function onSaveTiddlyWiki(event) {
     tbackup = "false";
-    if (data.bstrategy == "timed") {
+    if (timimisettings.bstrategy == "timed") {
       var now = new Date();
       var diffMs = now - tlast;
       var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
-      if (diffMins >= data.tint) {
+      if (diffMins >= timimisettings.tint) {
         tbackup = "true";
         tlast = now;
         console.log("Timimi: Creating Timed Backup");
       }
     }
+
     // Get the details from the message
     var messageElement = event.target,
       path = messageElement.getAttribute("data-tiddlyfox-path"),
       content = messageElement.getAttribute("data-tiddlyfox-content"),
       backupPath = messageElement.getAttribute("data-tiddlyfox-backup-path"),
       messageId = "tiddlywiki-save-file-response-" + idGenerator++;
+
     // Send the details to background script. Not using port because we need a promise and port.postMessage is not a promise
     var sending = browser.runtime.sendMessage({
       path: path,
       messageId: idGenerator,
       content: content,
       backupPath: backupPath,
-      backup: data.backup,
-      bpath: data.bpath,
-      bstrategy: data.bstrategy,
-      tohrecent: data.tohrecent,
-      tohlevel: data.tohlevel,
-      psint: data.psint,
+      backup: timimisettings.backup,
+      bpath: timimisettings.bpath,
+      bstrategy: timimisettings.bstrategy,
+      tohrecent: timimisettings.tohrecent,
+      tohlevel: timimisettings.tohlevel,
+      psint: timimisettings.psint,
       tbackup: tbackup
     });
-    sending.then(handleResponse, handleError);
+    sending.then(handleSent, handleError);
 
-    function handleResponse(message) {
+    function handleSent(message) {
       messageElement.parentNode.removeChild(messageElement);
       console.log("Message ID is " + messageId);
       var event = document.createEvent("Events");
@@ -112,54 +131,6 @@ if (checkTWResults.isTiddlyWiki && checkTWResults.isLocalFile) {
       console.log(`Error: ${error}`);
     }
   }
-  function onActionLaunch(event) {
-    console.log("Timimi: Received event: " + event.type);
-    indata = event.detail;
-    var LaunchElement = event.target;
-    console.log("Timimi: Launching event" + event.detail.escript);
-    var sending = browser.runtime.sendMessage({
-      exec: data.exec,
-      escript: event.detail.escript,
-      eparam: event.detail.eparam,
-      estdin: event.detail.estdin
-    });
-    sending.then(launchResponse, launchError);
-    function launchResponse(message) {
-      LaunchElement.parentNode.removeChild(LaunchElement);
-      console.log("Timimi: Launch script event concluded");
-    }
-
-    function launchError() {
-      console.log(`Timimi: Launch script event error: ${error}`);
-    }
-  }
-
 }
 
-if (checkTWResults.isTiddlyWikiClassic && checkTWResults.isLocalFile) {
-  injectExtensionScript('patch-classic-io.js');
-}
 
-browser.runtime.onMessage.addListener(request => {
-  console.log("Timimi: Received stdout in content-script");
-  var outdata = cloneInto(
-    {
-      message: request.stdout,
-      title: indata.title,
-      sep: indata.sep,
-      fields: indata.fields,
-      creationFields: indata.creationFields,
-      modificationFields: indata.modificationFields
-    },
-    document.defaultView
-  ); //Firefox Specific. See https://stackoverflow.com/a/46081249/7393623
-  var event = new CustomEvent("timimi-launch-script-stdout", {
-    detail: outdata,
-    bubbles: true,
-    cancelable: true
-  });
-
-  document.dispatchEvent(event);
-  console.log("Timimi: Dispatched Event timimi-launch-script-stdout");
-  // stdouttitle = "";
-});
